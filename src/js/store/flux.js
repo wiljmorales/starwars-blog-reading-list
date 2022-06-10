10a27184a9f062469a40b60c195775222f8f99ce
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-06-10 00:27:49 +0000

    doing fetch

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,31 @@
+const API_URL = "https://www.swapi.tech/api/"
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			characters: [],
+			planets: [],
+			vehicles: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			getCharacters: async () => {
+					try {
+						const response = await fetch(
+						`https://www.swapi.tech/api/people`
+					)
+					const body = await response.json()
+					if (response.status !==200) {
+						alert("no pudimos cargar los personajes")
+						return
+					}
+					setStore({
+						characters: body.results
+					})
+				}
+				catch(error) {
+					alert("fallamos ='(")
+					console.log(error)
+				} 
 			}
 		}
 	};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,22 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext } from "react";
 import "../../styles/home.css";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const {store, actions} = useContext(Context)
+	return (
+		<div className="text-center mt-5">
+			<div>
+				{store.characters.map(
+					(character) => {
+						return (
+						<div key={character.uid}>
+							{character.name}
+						</div>
+						)
+					}
+				)}
+			</div>
+		</div>
+	)
+};
