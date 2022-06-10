7dece2ff10f614e4de655368658a65e24ed10c0f
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-06-10 00:40:04 +0000

    fixing fetch url

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -11,7 +11,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			getCharacters: async () => {
 					try {
 						const response = await fetch(
-						`https://www.swapi.tech/api/people`
+						`${API_URL}people`
 					)
 					const body = await response.json()
 					if (response.status !==200) {
