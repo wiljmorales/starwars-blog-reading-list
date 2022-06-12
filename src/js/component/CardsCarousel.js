4669126ddb8a090b0ab77037559a8f04ea22628c
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-06-12 23:09:38 +0000

    Styling nav bar

--- src/js/component/CardsCarousel.js
+++ src/js/component/CardsCarousel.js
@@ -1,12 +1,10 @@
-import React from "react" 
+import React from "react";
 
-export const CardsCarousel = ({title, ...props}) => {
+export const CardsCarousel = ({ title, ...props }) => {
   return (
     <div className="container-fluid mb-5 px-5">
       <h2 className="text-danger text-start mb-5">{title}</h2>
-      <div className="d-flex overflow-auto">
-        {props.children}
-      </div>
+      <div className="d-flex overflow-auto">{props.children}</div>
     </div>
-  )
-}
+  );
+};
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,45 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light mb-3 pe-5">
+      <Link to="/" className="w-25 ms-5">
+        <img
+          src="https://webstockreview.net/images/starwars-clipart-logo-4.png"
+          className="w-50"
+          style={{
+            maxWidth: "100px",
+          }}
+        />
+      </Link>
+      <div class="dropdown me-4">
+        <button
+          class="btn btn-primary dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton1"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Favorites
+        </button>
+        <ul class="dropdown-menu me-5" aria-labelledby="dropdownMenuButton1">
+          <li>
+            <a class="dropdown-item" href="#">
+              Action
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">
+              Another action
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">
+              Something else here
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
